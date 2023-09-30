cfca492115400cad46be1dbf76558b2cabd2cbf1
Juan <43722131+JuanTecedor@users.noreply.github.com>
2023-09-30 22:09:49 +0200

    Add tests

---
+++ test/eng/math/TestAngle.cpp
@@ -0,0 +1,12 @@
+#include "../../../src/eng/math/Angle.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    constexpr auto number = 2;
+    REQUIRE( number == 2 );
+}
+
+TEST_CASE( "Error" ) {
+    REQUIRE(false);
+}
